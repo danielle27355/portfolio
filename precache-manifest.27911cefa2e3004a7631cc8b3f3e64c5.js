2a56ecb0591126f37428810df63086116dbcb266
danielle27355 <danielle27355@gmail.com>
2019-07-21 18:49:02 -0700

    Updates

---
+++ precache-manifest.27911cefa2e3004a7631cc8b3f3e64c5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ed284669e793ef4bc0523d1c0fa8858",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e98f552912ce0c8e893a",
+    "url": "/static/css/main.a9386d0f.chunk.css"
+  },
+  {
+    "revision": "26fa7c70e533745cdf11",
+    "url": "/static/js/2.c584e6ec.chunk.js"
+  },
+  {
+    "revision": "e98f552912ce0c8e893a",
+    "url": "/static/js/main.7f241d5f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7e3753d38a5cb23e5c0070f1965ff676",
+    "url": "/static/media/debussy.7e3753d3.jpg"
+  },
+  {
+    "revision": "f55c2e80a952ca05467a7ba09b331304",
+    "url": "/static/media/logo.f55c2e80.png"
+  },
+  {
+    "revision": "8a9823eaf2d6a2b025628fdb84f08cde",
+    "url": "/static/media/mastodon.8a9823ea.jpg"
+  },
+  {
+    "revision": "60c826e6951471c3d6f0e5290c47a21e",
+    "url": "/static/media/me.60c826e6.jpg"
+  },
+  {
+    "revision": "ec671fcf24c83c1b9221920949b3b991",
+    "url": "/static/media/slayer.ec671fcf.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.30cde41786250970ce0979db3efb82a3.js"
+  "/precache-manifest.27911cefa2e3004a7631cc8b3f3e64c5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
